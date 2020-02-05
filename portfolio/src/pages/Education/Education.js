a2bd7f7e7ede94790014deb489bd483c3d19cfb7
Sara Alkhamri <s.alkhamri@gmail.com>
2020-02-04 20:07:27 -0800

    Deleted a comma in Education.js

--- portfolio/src/pages/Education/Education.js
+++ portfolio/src/pages/Education/Education.js
@@ -14,7 +14,7 @@ const Education = ({ user }) => {
               <Institution>{education.position}</Institution>
               <div>
                 <Degree>
-                  {education.studyType}, {education.area}
+                  {education.studyType} {education.area}
                 </Degree>{' '}
                 <span> &sdot; </span>
                 <span>
